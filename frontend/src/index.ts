d4ce81add8b0eb263ef740bb1e5e0b26a1b4a160
electronfraud <electronfraud@gmail.com>
2025-05-31 16:43:49 -0700

    Add a zoom-to-fit button

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -16,6 +16,60 @@ function updateURLAnchor(map: mapboxgl.Map): void {
     history.replaceState(null, "", url);
 }
 
+class ZoomToFitControl {
+    map?: mapboxgl.Map;
+    aircraftLayer?: AircraftLayer;
+
+    onAdd(map: mapboxgl.Map): HTMLElement {
+        this.map = map;
+
+        const container = document.createElement("div");
+        container.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
+
+        const button = document.createElement("button");
+        button.onclick = this.#zoom.bind(this);
+        button.className = "custom-ctrl-zoom-to-fit";
+        button.ariaLabel = "Zoom to fit all aircraft";
+        container.appendChild(button);
+
+        const icon = document.createElement("span");
+        icon.className = "mapboxgl-ctrl-icon";
+        button.appendChild(icon);
+
+        return container;
+    }
+
+    onRemove() {
+        this.map = undefined;
+    }
+
+    #zoom() {
+        const features = this.aircraftLayer?.features ?? [];
+        if (features.length == 0) {
+            return;
+        }
+
+        let minX = 180;
+        let minY = 90;
+        let maxX = -180;
+        let maxY = -90;
+        for (const feature of this.aircraftLayer?.features ?? []) {
+            if (feature.x < minX) minX = feature.x;
+            if (feature.y < minY) minY = feature.y;
+            if (feature.x > maxX) maxX = feature.x;
+            if (feature.y > maxY) maxY = feature.y;
+        }
+
+        this.map?.fitBounds(
+            [
+                [minX, minY],
+                [maxX, maxY]
+            ],
+            { padding: 100 }
+        );
+    }
+}
+
 async function main() {
     // extract location, zoom, and bearing from URL hash
     let centerLon = 0;
@@ -43,9 +97,13 @@ async function main() {
         minPitch: 0
     });
 
-    map.addControl(new mapboxgl.FullscreenControl());
-    map.addControl(new mapboxgl.NavigationControl());
-    map.addControl(new mapboxgl.ScaleControl({ unit: "nautical" }));
+    map.addControl(new mapboxgl.FullscreenControl(), "top-right");
+    map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), "bottom-right");
+    map.addControl(new mapboxgl.NavigationControl({ showZoom: false }), "bottom-right");
+    map.addControl(new mapboxgl.GeolocateControl({ trackUserLocation: false }), "bottom-right");
+    const zoomToFitControl = new ZoomToFitControl();
+    map.addControl(zoomToFitControl, "bottom-right");
+    map.addControl(new mapboxgl.ScaleControl({ unit: "nautical" }), "bottom-left");
 
     map.on("zoomend", (_) => {
         updateURLAnchor(map);
@@ -60,6 +118,7 @@ async function main() {
     // add the aircraft layer
     await addAircraftSymbols(map);
     const aircraftLayer = new AircraftLayer("aircraft", map);
+    zoomToFitControl.aircraftLayer = aircraftLayer;
 
     // subscribe to updates
     new AggregatorClient(aircraftLayer, document.getElementById("staleness") as StaleIndicator);
